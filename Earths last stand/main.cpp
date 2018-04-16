d710de0af25b677e6ed5291419f54b1475ef48f1
kyle5009 <32904816+kyle5009@users.noreply.github.com>
2018-04-16 11:36:01 +0100

    1.0

--- Earths last stand/main.cpp
+++ Earths last stand/main.cpp
@@ -1,8 +1,3 @@
-file(GLOB_RECURSE SOURCES 2_enemys/*.cpp 2_enemys/*.h)
-add_excecutable(2_ENEMYS ${SOURCES} )
-target_include_directories(2_INVADERS SYSTEM PRIVATE ${SFML_INCS})
-target_link_libraries(2_ENEMYS sfml-graphics)
-
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "railgun.h"
@@ -21,6 +16,11 @@ void Load() {
    }
    enemy.setTexture(spritesheet);
    invader.setTextureRect(sf::IntRect(0,0,32,32));
+}
+
+
+void Render() {
+  window.draw(enemy):
 }
 
 
